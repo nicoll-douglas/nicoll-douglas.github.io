eec798c9be4506c373fcc73fe4c11b68e024a410
nicoll-douglas <nicoll.leo.douglas@gmail.com>
2024-05-14 16:39:08 +0100

    complete studium page

--- src/pages/projects/Studium/Studium.jsx
+++ src/pages/projects/Studium/Studium.jsx
@@ -1,14 +1,14 @@
+import UnderConstruction from "../../../components/UnderConstruction";
+
 export default function Studium() {
   return (
     <section className="flex flex-col">
       <h1 className="text-3xl text-teal-700">Studium</h1>
       <p>
         A frontend web application I{"'"}m currently working on, stay tuned for
-        updates!
+        updates :-)
       </p>
-      <div>
-        <img src="/animated/under-construction-1.gif" aria-hidden="true" />
-      </div>
+      <UnderConstruction />
     </section>
   );
 }
