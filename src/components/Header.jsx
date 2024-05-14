0265f5724ef021c78d9df0c1280616df65b15cce
nicoll-douglas <nicoll.leo.douglas@gmail.com>
2024-05-14 13:23:50 +0100

    create reusable header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header({ children, background }) {
+  return (
+    <div
+      className={`flex items-center justify-center window ${background} py-4 lg:px-4 w-full`}
+    >
+      {children}
+    </div>
+  );
+}
