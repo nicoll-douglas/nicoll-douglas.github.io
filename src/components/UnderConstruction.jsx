c5c768c80805067023087121d2fe53d80995a727
nicoll-douglas <nicoll.leo.douglas@gmail.com>
2024-05-14 16:38:57 +0100

    add under construction component

---
+++ src/components/UnderConstruction.jsx
@@ -0,0 +1,15 @@
+export default function UnderConstruction() {
+  return (
+    <div className="flex flex-wrap gap-2 items-center">
+      <img src="/animated/under-construction-1.gif" aria-hidden="true" />
+      <img src="/animated/under-construction-2.gif" aria-hidden="true" />
+      <img src="/animated/under-construction-3.gif" aria-hidden="true" />
+      <img src="/animated/under-construction-4.gif" aria-hidden="true" />
+      <img src="/animated/under-construction-5.gif" aria-hidden="true" />
+      <img src="/animated/under-construction-6.gif" aria-hidden="true" />
+      <img src="/animated/under-construction-7.gif" aria-hidden="true" />
+      <img src="/animated/under-construction-8.gif" aria-hidden="true" />
+      <img src="/animated/under-construction-9.gif" aria-hidden="true" />
+    </div>
+  );
+}
