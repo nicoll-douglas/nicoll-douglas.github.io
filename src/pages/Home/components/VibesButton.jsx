d27e03a11853e132e71095bfbcc3e045519d4eed
nicoll-douglas <nicoll.leo.douglas@gmail.com>
2024-05-22 00:03:18 +0100

    fix vibes button

--- src/pages/Home/components/VibesButton.jsx
+++ src/pages/Home/components/VibesButton.jsx
@@ -1,7 +1,7 @@
 export default function VibesButton() {
   return (
     <a
-      href="https://open.spotify.com/playlist/73lRri4Fzu21ButMD0DtYv?si=ff99812a06bb4716&pt=85c02d3dd83caf72e01f538310bc5acc"
+      href="https://open.spotify.com/playlist/73lRri4Fzu21ButMD0DtYv"
       target="_blank"
       className=" text-black text-nowrap button p-4 mt-auto"
       style={{
