285448a35846b836247c46fe78cce2d09c5b53a2
nicoll-douglas <nicoll.leo.douglas@gmail.com>
2024-05-26 17:42:55 +0100

    adjust layout styles

--- src/pages/projects/Studium/Studium.jsx
+++ src/pages/projects/Studium/Studium.jsx
@@ -2,7 +2,7 @@ import UnderConstruction from "../../../components/UnderConstruction";
 
 export default function Studium() {
   return (
-    <section className="flex flex-col">
+    <section className="flex flex-col flex-grow w-full">
       <h1 className="text-3xl text-teal-700">Studium</h1>
       <p>
         A frontend web application I{"'"}m currently working on, stay tuned for
