85afb3448ba4e0afad45939c66fab7bafc7f9d59
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-10-07 22:48:34 +0100

    content: edit

--- src/js/collages.js
+++ src/js/collages.js
@@ -1,1 +1,16 @@
+import "../css/collages.css";
+const images = import.meta.glob("@/assets/images/collages/blue/*");
+const images2 = import.meta.glob("@/assets/images/collages/orange/*");
+
 import appendMany from "./common/appendMany";
+
+const container = document.querySelector("#blue-collage");
+const container2 = document.querySelector("#orange-collage");
+
+const imgCallback = (img) => {
+  img.classList.add("collage-image");
+  img.classList.add("window");
+};
+
+appendMany(container, images, imgCallback);
+appendMany(container2, images2, imgCallback);
