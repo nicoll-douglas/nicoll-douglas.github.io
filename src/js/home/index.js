6766fc9ae3e1321dbc2cc4fb70de77aa8e463d63
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-10-12 23:50:20 +0100

    content/style: edit

--- src/js/home/index.js
+++ src/js/home/index.js
@@ -5,3 +5,9 @@ import "./createButtons";
 import "./createBlinkies";
 import "./songSelectionModal";
 import "./timeSinceY2k";
+
+const dangerZone = document.querySelector("#danger-zone");
+setInterval(() => {
+  dangerZone.classList.toggle("text-white");
+  dangerZone.classList.toggle("text-black");
+}, 750);
